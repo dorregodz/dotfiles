33b240b13e2d6a2309d79f417f7c8b630a539df8
Diego Dorrego Díaz <diego.dorrego.diaz@gmail.com>
2023-03-20 22:45:54 +0100

    Add vscode settings
    keybinds: Added keybinds to create files and folders
    settings:
    - Update editor line number bar width
    - Disable breadcrumbs
    - Configure themes

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,8 @@
 # Home config files
 ln -s $DOTFILES_PATH/shell/zsh/.zshrc $HOME/.zshrc 
 ln -s $DOTFILES_PATH/git/.gitconfig $HOME/.gitconfig 
+
+# Visual Studio Code config files
+ln -s $DOTFILES_PATH/editors/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -s $DOTFILES_PATH/editors/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $DOTFILES_PATH/editors/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
