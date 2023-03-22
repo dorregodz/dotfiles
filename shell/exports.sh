52480033d0e91b58ce18a3ee21f88af61e094dc8
Diego Dorrego Díaz <diego.dorrego.diaz@gmail.com>
2023-03-22 17:43:12 +0100

    Add light and dark profile and switch them
    This change won't be deffinitive.
    It does not modify all iterm shells, only current
    Also is not a good practise the way it's done

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,3 +2,5 @@
 # Path - The higher it is, the more priority it has
 # ------------------------------------------------------------------------------
 export PATH="$PATH:$DOTFILES_PATH/bin:$DOTFILES_PATH/os/mac/bin"
+
+export ITERM_PROFILE="$(cat $DOTFILES_PATH/os/mac/iTerm2/iTerm_profile)"
