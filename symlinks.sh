e040d1d8b5add9b73b2237c85241ea8e383f4e01
Diego Dorrego Díaz <diego.dorrego.diaz@gmail.com>
2023-07-25 11:07:33 +0200

    Add intelliJ idea config

--- symlinks.sh
+++ symlinks.sh
@@ -4,6 +4,10 @@ ln -s $DOTFILES_PATH/git/.gitconfig $HOME/.gitconfig
 ln -s $DOTFILES_PATH/git/.gitignore_global $HOME/.gitignore_global 
 ln -s $DOTFILES_PATH/editors/vim/.vimrc .vimrc
 
+# IntelliJ config files
+# Sync manually: $DOTFILES_PATH/editors/idea/idea.properties /Applications/IntelliJ\ IDEA\ CE.app/Contents/bin/idea.properties
+# Sync manually: $DOTFILES_PATH/editors/idea/idea.vmoptions /Applications/IntelliJ\ IDEA\ CE.app/Contents/bin/idea.vmoptions
+
 # Visual Studio Code config files
 ln -s $DOTFILES_PATH/editors/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
 ln -s $DOTFILES_PATH/editors/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
