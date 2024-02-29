89eb56b804b0d6a9382c3e02b69549b0286bc620
Diego Dorrego Díaz <diego.dorrego.diaz@gmail.com>
2024-02-29 17:37:04 +0100

    Add some util aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,3 +1,13 @@
 alias dot='code ~/.dotfiles'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
+
+# Docker utils
+alias d='docker'
+alias dps='docker ps'
+alias dcupd='docker-compose up -d'
+alias dcdn='docker-compose down'
+
+# Maven wrapper utils
+alias mwci='./mvnw clean install'
+alias mwrun='./mvnw spring-boot:run'
