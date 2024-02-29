1027593c71219ad49470bee7776f444b2b09e902
Diego Dorrego Díaz <diego.dorrego.diaz@gmail.com>
2024-02-29 17:43:32 +0100

    Add install script with some of the installed tools
    This script would be used to configure a new PC

---
+++ os/mac/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+brew install jq
+brew install fzf
+brew install iterm2 --cask
+
+## Other
+brew install --cask postman
+
+# Install IDEs
+brew install --cask intellij-idea-ce
+brew install --cask visual-studio-code
+brew install zed
+
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+brew install coreutils curl git
+brew install gpg gawk
